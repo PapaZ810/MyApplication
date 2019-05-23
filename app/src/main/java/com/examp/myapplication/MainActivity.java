96763952febdae7124b19464517fbfc0f61ff48f
Zac <zombiezac@att.net>
2019-05-23 13:53:46 -0600

    app that changes colors

---
+++ app/src/main/java/com/examp/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.examp.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ConstraintLayout layout = findViewById(R.id.layout);
+
+        Button button = findViewById(R.id.change);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int red = (int) (Math.random() * 255);
+                int blue = (int)(Math.random() * 255);
+                int green = (int) (Math.random() * 255);
+
+                layout.setBackgroundColor(Color.rgb(red, green, blue));
+            }
+        });
+    }
+}
